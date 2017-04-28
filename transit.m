4d099576e9e17520f9ddffcc0c2ba79d99b2250e
XinweiChai <xinwei.chai@hotmail.com>
2017-04-28 18:00:42 +0200

    Delete transit.m

--- transit.m
+++
@@ -1,8 +0,0 @@
-function [state,modified]=transit(currentState,action,process)
-    state=currentState;
-    modified=0;
-    if isFirable(currentState,action,process)
-        state{process(action{2}),2}=action{4};
-        modified=1;
-    end
-end
