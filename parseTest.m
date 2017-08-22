5ee0dc53206f0e279c145526f7a26d8d8cb38fa0
Xinwei Chai <xinwei.chai@hotmail.com>
2017-08-22 19:01:33 +0200

    parseTest

--- parseTest.m
+++ parseTest.m
@@ -1,13 +1,38 @@
+%Read the .out file of pint test
+
 clc;clear;
-f=fopen(fn,'r');
+f=fopen('run-egfr104-priority-2015-02-12.out','r');
+%killed=9986;
+%Inconc=zeros(killed,12);
+Inconc=[];
 %Beginning information
 while ~feof(f)
     tline = fgetl(f);
     if strcmp(tline(1:3),'---')
+        temp=tline(5:end);
+        words=regexp(temp,',*\s|\(\*|\*\)','split');
+        dict=containers.Map(1:13,words(1:2:25)); %dictionary of varying initial states
         break;
     end
 end
 %Data sets
+posVert=0;
 while ~feof(f)
-    
+    killedMark=0;
+    temp=tline(5:end);
+    if strcmp(tline(1:3),'---')
+        for i=1:12
+            tline = fgetl(f);
+            tline = fgetl(f);
+            if strcmp(tline,'*** Killed ***')
+                killedMark=1;
+            end
+        end
+        if killedMark
+            posVert=posVert+1;
+            words=regexp(temp,',*\s|\(\*|\*\)','split');
+            Inconc=[Inconc;words(2:2:end)];
+        end
+    end
+    tline = fgetl(f);
 end
