3b52b578b2560b85b0ff684542f1831b653d3479
Xinwei Chai <xinwei.chai@hotmail.com>
2017-09-05 20:01:49 +0200

    SLCG

--- SLCG.m
+++ SLCG.m
@@ -1,12 +1,34 @@
-function lcg=SLCG(initialState, actions, startNode)
-if ismember(startNode,traversed)
-    return;
+function initialNode=SLCG(initialState, actions, startNode)
+%node=(type, name, state) e.g. initialState=(0,1,0) (stateNode, 1st sort, state 0)
+%sol=(1,1) (solutionNode, 1st action) 0 stands for trivial solution
+%Need a dictionary for names of sorts (finished)
+%action=({hitter(s), hitterState(s)}, target, targetState)
+Ls=startNode;
+LS=initialState;
+initialNode=nlnode(initialState);
+while ~isempty(Ls)
+    for i=Ls
+        if ismember(i,LS)
+            continue;
+        end
+        Ls=Ls(2:end);
+        curr=nlnode(i);
+        if i==initialState
+            tempSol=[1,0];
+            temp=nlnode(tempSol);
+            insertAfter(temp,curr);
+        else
+            act=findSol(i,actions);
+            for j=act
+                tempSol=nlnode(j);
+                insertAfter(j,curr);
+                for k=j{1}
+                    tempSol.Next=k;
+                end
+                Ls=[Ls,k];
+                LS=union(LS,ls);
+            end
+        end
+    end
 end
-while ls 
-%     traversed=startNode;
-%     temp=startNode;
-%     for i=temp
-%         find(actions(2),startNode)
-%     end
-%
 end
---
+++ test.m
@@ -0,0 +1,6 @@
+clear
+n1=dlnode(1);
+n2=dlnode(2);
+n3=dlnode(3);
+insertAfter(n3,n1);
+insertAfter(n2,n1);
