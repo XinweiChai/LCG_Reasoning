fd1a943f81457bf94e18abbe8a3872bfb998f922
XinweiChai <xinwei.chai@hotmail.com>
2017-09-10 22:12:34 +0200

    inconcTest

--- parseTest.m
+++ parseTest.m
@@ -1,27 +1,32 @@
 %Read the .out file of pint test
-
-clc;clear;
-f=fopen('data\\run-egfr104-priority-2015-02-12.out','r');
+function [inconc,dictInput,dictOutput]=parseTest(filename)
+f=fopen(filename,'r');
 %killed=9986;
 %Inconc=zeros(killed,12);
-Inconc=[];
+inconc=[];
 %Beginning information
+dictOutput=containers.Map;
 while ~feof(f)
     tline = fgetl(f);
-    if strcmp(tline(1:3),'---')
-        temp=tline(5:end);
-        words=regexp(temp,',*\s|\(\*|\*\)','split');
-        dict=containers.Map(1:13,words(1:2:25)); %dictionary of varying initial states
+    if strcmp(tline(1:6),'Inputs')
+        words=regexp(tline,'\s','split');
+        count=size(words,2);
+        dictInput=containers.Map(1:count-1,words(2:count));
+    elseif strcmp(tline(1:7),'Outputs')
+        words=regexp(tline,'\s','split');
+        count=size(words,2);
+        dictOutput=containers.Map(1:count-1,words(2:count));
         break;
     end
 end
+
 %Data sets
 posVert=0;
 while ~feof(f)
     killedMark=0;
     temp=tline(5:end);
     if strcmp(tline(1:3),'---')
-        for i=1:12
+        for i=1:size(dictOutput,1)
             tline = fgetl(f);
             tline = fgetl(f);
             if strcmp(tline,'*** Killed ***')
@@ -31,8 +36,9 @@ while ~feof(f)
         if killedMark
             posVert=posVert+1;
             words=regexp(temp,',*\s|\(\*|\*\)','split');
-            Inconc=[Inconc;words(2:2:end)];
+            inconc=[inconc;str2double(words(2:2:end))];
         end
     end
     tline = fgetl(f);
+end
 end
